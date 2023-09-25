46dbfe93256c58640d2623e3247a8d36c614f2ff
Mauregina <mauraregina@gmail.com>
2023-09-25 18:19:28 -0400

    create a context to cart list

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,31 @@ import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { createContext, useState } from 'react'
+
+interface Cart {
+  coffeeId: number
+  quantity: number
+}
+
+interface CartContextType {
+  cart: Cart[]
+}
+
+export const CartContext = createContext({} as CartContextType)
 
 export function App() {
+  const [cartItems] = useState<Cart[]>([
+    { coffeeId: 1, quantity: 1 },
+    { coffeeId: 2, quantity: 1 },
+  ])
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CartContext.Provider value={{ cart: cartItems }}>
+          <Router />
+        </CartContext.Provider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,8 +10,14 @@ import {
 } from './styles'
 
 import Logo from '../../assets/logo.svg'
+import { useContext } from 'react'
+import { CartContext } from '../../App'
 
 export function Header() {
+  const { cart } = useContext(CartContext)
+
+  console.log('NA HEADER ', cart)
+
   return (
     <HeaderContainer>
       <Link to="/">
