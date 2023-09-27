2d5a91ad9bb3314acca48c1e4629c7cc9b44f7f8
Mauregina <mauraregina@gmail.com>
2023-09-27 16:29:02 -0400

    add coffee image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,16 +7,11 @@ import {
 
 import { Intro } from './components/Intro'
 import { CoffeeItem } from './components/CoffeeItem'
-import { useContext, useEffect } from 'react'
-import { CartContext, MenuContext } from '../../App'
+import { useContext } from 'react'
+import { MenuContext } from '../../App'
 
 export function Home() {
   const { menu } = useContext(MenuContext)
-  const { cart } = useContext(CartContext)
-
-  useEffect(() => {
-    console.log(cart)
-  }, [])
 
   return (
     <HomeContainer>
